da41ebf48b00738bc425f921e6df45ab436ae7cc
Tung Nguyen <tungntt.3108@gmail.com>
2020-11-30 15:42:15 +0700

    initialize project

---
+++ src/test/java/vn/tungnt/interview/VehicleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tungnt.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
