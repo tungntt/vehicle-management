a3598d97508ed0c407b773b6e1fc1a9f7bb449be
Tung Nguyen <tungntt.3108@gmail.com>
2020-12-09 17:34:24 +0700

    - fix to package running on production

--- src/main/java/vn/tungnt/interview/service/impl/PaymentServiceImpl.java
+++ src/main/java/vn/tungnt/interview/service/impl/PaymentServiceImpl.java
@@ -3,13 +3,10 @@ package vn.tungnt.interview.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import vn.tungnt.interview.domain.entity.DriverEntity;
-import vn.tungnt.interview.domain.entity.PaymentDetailEntity;
+import vn.tungnt.interview.domain.entity.*;
 import vn.tungnt.interview.domain.entity.PaymentDetailEntity.BusinessMan;
 import vn.tungnt.interview.domain.entity.PaymentDetailEntity.PaymentDetailId;
-import vn.tungnt.interview.domain.entity.PaymentEntity;
 import vn.tungnt.interview.domain.entity.PaymentEntity.PaymentStatus;
-import vn.tungnt.interview.domain.entity.VehicleEntity;
 import vn.tungnt.interview.domain.entity.VehicleEntity.VehicleStatus;
 import vn.tungnt.interview.repository.PaymentDetailRepository;
 import vn.tungnt.interview.repository.PaymentRepository;
@@ -74,8 +71,16 @@ public class PaymentServiceImpl extends AbstractService<PaymentEntity, PaymentDT
 
     @Override
     public PaymentDTO payTransferringBill(final CommitTransferringVehicleRequestDTO request) {
+        final CredentialEntity currentCredential = this.getCurrentCredential();
+        final DriverEntity driverInfo = currentCredential.getDriverInfo();
         final PaymentEntity payment = this.repository.findById(request.getTransactionId())
                 .orElseThrow(() -> new BusinessException(String.format("There is no transaction transferring vehicle with id %s", request.getBankAccountId())));
+        final PaymentDetailEntity paymentOfBuyer = payment.getTransactionDetails().stream()
+                .filter(t -> t.getBusinessMan().equals(BusinessMan.BUYER))
+                .findFirst().orElseThrow(() -> new BusinessException("Invalid transferring vehicle transaction. Due to miss buyer!!"));
+        if (!driverInfo.getId().equals(paymentOfBuyer.getDriver().getId())) {
+            throw new BusinessException("The buyer must be the one submitting this payment!!");
+        }
         payment.setStatus(PaymentStatus.PAYMENT_DONE);
         payment.getTransferredVehicle().setStatus(VehicleStatus.ACTIVE);
         this.repository.save(payment);
